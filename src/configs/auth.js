24bf2cc715485def0cb71adfd19d068de58db6c6
Sayedur Rahman <s.rahman3443@gmail.com>
2024-04-30 23:27:45 +0600

    project added

--- src/configs/auth.js
+++
@@ -1,7 +0,0 @@
-export default {
-  meEndpoint: '/auth/me',
-  loginEndpoint: '/api/login',
-  registerEndpoint: '/jwt/register',
-  storageTokenKeyName: 'accessToken',
-  onTokenExpiration: 'refreshToken' // logout | refreshToken
-}
