ecf444d0f742117b800b83c8daccd811c7bb3177
Sayedur Rahman <s.rahman3443@gmail.com>
2024-05-03 18:55:37 +0600

    dashboard implemented

--- src/store/modules/adminLogin/adminLoginSlice.js
+++ src/store/modules/adminLogin/adminLoginSlice.js
@@ -116,19 +116,16 @@ export const updateUser = createAsyncThunk('adminLoginSlice/updateUser', async (
     });
 });
 
-export const getUserInfo = (token) => {
-  return async () => {
-    const fetchData = async () => {
-      const header = { headers: { Authorization: 'Bearer ' + token } };
-      const response = await axios.post(process.env.REACT_APP_API_URL + 'api/user-info', null, header);
-      return await response.data;
-    };
-    try {
-      return await fetchData();
-    } catch (error) {
-      throw error.response;
-    }
-  };
-};
+export const getStat = createAsyncThunk('adminLoginSlice/getStat', async (data) => {
+  const header = { headers: { 'Content-Type': 'application/json', Authorization: 'Bearer ' + data.token } };
+  return await axios
+    .get(process.env.REACT_APP_API_URL + 'api/stats/', null, header)
+    .then(function (response) {
+      return response.data;
+    })
+    .catch(function (error) {
+      return error;
+    });
+});
 
 export default adminLoginSlice.reducer;
--- src/views/dashboard/Default/index.js
+++ src/views/dashboard/Default/index.js
@@ -1,34 +1,126 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
-// material-ui
 import { Grid } from '@mui/material';
 
-// project imports
-import EarningCard from './EarningCard';
-import TotalOrderLineChartCard from './TotalOrderLineChartCard';
-import { gridSpacing } from 'store/constant';
+import Button from '@mui/joy/Button';
+import Card from '@mui/joy/Card';
+import CardActions from '@mui/joy/CardActions';
+import CardContent from '@mui/joy/CardContent';
+import CircularProgress from '@mui/joy/CircularProgress';
+import { CssVarsProvider as JoyCssVarsProvider } from '@mui/joy/styles';
+import Typography from '@mui/joy/Typography';
+import CssBaseline from '@mui/material/CssBaseline';
+import { IconForms } from '@tabler/icons-react';
+import { Link } from 'react-router-dom';
+import { getStat } from 'store/modules/adminLogin/adminLoginSlice';
+import AuthContext from 'store/modules/authContext';
+import { useAppDispatch } from 'store/reducer';
 
 // ==============================|| DEFAULT DASHBOARD ||============================== //
 
 const Dashboard = () => {
-  const [isLoading, setLoading] = useState(true);
+  const [stats, setStats] = useState({});
+  const dispatch = useAppDispatch();
+  const authCtx = useContext(AuthContext);
+
   useEffect(() => {
-    setLoading(false);
-  }, []);
+    const fetchData = async () => {
+      const response = await dispatch(
+        getStat({
+          token: authCtx.currentUser.token
+        })
+      );
+      if (response.payload && response.payload.status && response.payload.status === 'success' && response.payload.data) {
+        setStats(response.payload.data);
+      }
+    };
+
+    fetchData();
+  }, [dispatch, authCtx.currentUser.token, setStats]);
 
   return (
-    <Grid container spacing={gridSpacing}>
-      <Grid item xs={12}>
-        <Grid container spacing={gridSpacing}>
-          <Grid item lg={4} md={6} sm={6} xs={12}>
-            <EarningCard isLoading={isLoading} />
+    <>
+      <JoyCssVarsProvider>
+        <CssBaseline enableColorScheme />
+        <Grid container sx={{ gap: 2, flexWrap: 'wrap', p: 0, m: 0 }}>
+          <Grid item>
+            <Card variant="solid" color="primary" invertedColors sx={{ flexGrow: 1 }}>
+              <CardContent orientation="horizontal">
+                <CircularProgress size="lg" determinate>
+                  <IconForms />
+                </CircularProgress>
+                <CardContent>
+                  <Typography level="body-md">Total Requests</Typography>
+                  <Typography level="h2">{stats.all_leaves || 0}</Typography>
+                </CardContent>
+              </CardContent>
+              <CardActions>
+                <Button variant="soft" size="sm" component={Link} to="/leave-request/leave-list/all">
+                  View
+                </Button>
+              </CardActions>
+            </Card>
           </Grid>
-          <Grid item lg={4} md={6} sm={6} xs={12}>
-            <TotalOrderLineChartCard isLoading={isLoading} />
+
+          <Grid item>
+            <Card variant="solid" color="warning" invertedColors sx={{ flexGrow: 1 }}>
+              <CardContent orientation="horizontal">
+                <CircularProgress size="lg" determinate>
+                  <IconForms />
+                </CircularProgress>
+                <CardContent>
+                  <Typography level="body-md">Pending Requests</Typography>
+                  <Typography level="h2">{stats.pending_leaves || 0}</Typography>
+                </CardContent>
+              </CardContent>
+              <CardActions>
+                <Button variant="soft" size="sm" component={Link} to="/leave-request/leave-list/pending">
+                  View
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+
+          <Grid item>
+            <Card variant="solid" color="success" invertedColors sx={{ flexGrow: 1 }}>
+              <CardContent orientation="horizontal">
+                <CircularProgress size="lg" determinate>
+                  <IconForms />
+                </CircularProgress>
+                <CardContent>
+                  <Typography level="body-md">Approved Requests</Typography>
+                  <Typography level="h2">{stats.approved_leaves || 0}</Typography>
+                </CardContent>
+              </CardContent>
+              <CardActions>
+                <Button variant="soft" size="sm" component={Link} to="/leave-request/leave-list/approved">
+                  View
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+
+          <Grid item>
+            <Card variant="solid" color="danger" invertedColors sx={{ flexGrow: 1 }}>
+              <CardContent orientation="horizontal">
+                <CircularProgress size="lg" determinate>
+                  <IconForms />
+                </CircularProgress>
+                <CardContent>
+                  <Typography level="body-md">Rejected Requests</Typography>
+                  <Typography level="h2">{stats.rejected_leaves || 0}</Typography>
+                </CardContent>
+              </CardContent>
+              <CardActions>
+                <Button variant="soft" size="sm" component={Link} to="/leave-request/leave-list/rejected">
+                  View
+                </Button>
+              </CardActions>
+            </Card>
           </Grid>
         </Grid>
-      </Grid>
-    </Grid>
+      </JoyCssVarsProvider>
+    </>
   );
 };
 
